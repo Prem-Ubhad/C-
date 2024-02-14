7034e00fb4da49e0c8104b37eaba6144e8d00332
Prem-Ubhad <coderprem0@gmail.com>
2024-02-14 11:54:26 +0530

    cpp

--- tut6.cpp
+++ tut6.cpp
@@ -2,6 +2,30 @@
 using namespace std;
 int main()
 {
+   int i = 1;
    
+   //For Loop
+   for (int i = 0; i < 40; i++)
+   {
+      cout<<i<<endl;
+   }
+   
+
+  // While Loop
+  while (i<40)
+  {
+   cout<<i<<endl;
+   i++;
+  }
+
+// do-while Loop
+do
+{
+   cout<<i<<endl;
+   i++;
+} while (i<=40);
+
+  
+
    return 0;
 }
---
+++ tut7.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   // MULTIPLICATION TABLE OF 6
+   cout<<"********* MULTIPLICATION TABLE OF 6 *********"<<endl<<endl;
+
+   for (int i = 1; i <= 10; i++)
+   {
+    cout<< "6 * " << i << " = " << 6 * i <<endl;
+     
+   }
+   
+
+   return 0;
+}
